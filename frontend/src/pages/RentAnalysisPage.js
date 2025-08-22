3e641a49b9381d0349c71de2e8cdd62140c7a125
edwardhe10 <88951771+edwardhe10@users.noreply.github.com>
2025-08-22 16:25:38 -0400

    city selector fix

--- frontend/src/pages/RentAnalysisPage.js
+++ frontend/src/pages/RentAnalysisPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import CitySelector from "../components/CitySelector";
-import TableauViz from "../components/TableauViz"; // ready to use when you embed
+//import TableauViz from "../components/TableauViz"; // ready to use when you embed
 
 export default function RentAnalysisPage() {
   const [city, setCity] = useState("");
@@ -14,13 +14,19 @@ export default function RentAnalysisPage() {
 
       <div className="panel placeholder">
         <h3>Average Rent Over Time</h3>
-        {<TableauViz src="https://public.tableau.com/views/housing-affordability-analysis-dashboard/Dashboard1" cityFilterField="City" cityValue={city} /> }
+        { <tableau-viz id="tableauViz"
+            style={{ width: "100%", height: "100vh" }}
+            device="desktop"       
+            key={city}
+            src='https://public.tableau.com/views/RentDataVisualization/Dashboard1'   
+            toolbar="bottom" hide-tabs>
+            <viz-filter field="City" value={city}> </viz-filter>    
+            </tableau-viz> }
         <p>📊 Placeholder: Rent trends for <b>{city}</b></p>
       </div>
 
       <div className="panel placeholder">
         <h3>Rent Distribution / Unit Types</h3>
-            <TableauViz src="https://public.tableau.com/views/RentDataVisualization/MedianRent" cityFilterField="City" cityValue={city} />
         <p>📈 Placeholder: Bar/Box plots by unit type for <b>{city}</b></p>
       </div>
     </section>
