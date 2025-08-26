8a971489d016fe1527c87d3a36b63c71b70e7838
edwardhe10 <88951771+edwardhe10@users.noreply.github.com>
2025-08-26 17:04:16 -0400

    finish income page, add key insights and data source

--- frontend/src/pages/IncomeAnalysisPage.js
+++ frontend/src/pages/IncomeAnalysisPage.js
@@ -1,19 +1,70 @@
 import React from "react";
 
 export default function IncomeAnalysisPage() {
-  return (
-    <section className="page">
-      <h2>Income Analysis</h2>
+    return (
+        <section className="page">
+            <div className="info-box">
+                <h2>Income Growth & Trends in Ontario</h2>
 
-      <div className="panel panel-content">
-        <tableau-viz id="tableauViz"
-            style={{ width: "100%", height: "100vh" }}
-            device="desktop"       
-            src='https://public.tableau.com/views/IncomeAnalysis_17557250314520/Dashboard'
-            toolbar="bottom"
-            hide-tabs>
-            </tableau-viz>
-      </div>
-    </section>
-  );
+                <h3>Why Income Analysis Matters</h3>
+                <p>
+                    Understanding income growth helps track affordability, cost-of-living pressures, 
+                    and regional disparities. By looking at median income trends in Ontario, 
+                    we can see how earnings have changed over time and how they compare to rental housing trends.
+                </p>
+
+                <h3>About</h3>
+                <p>
+                    This dashboard uses Statistics Canada data to display income from 2019 to 2023 in Ontario while 
+                    the rent prices uses the same data from the rent analysis. Median after-tax income was chosen to 
+                    most closely represent a renter’s income state when paying rent. Additionally, data was narrowed 
+                    down to persons not in economic families to represent the majority of individual renters as rent 
+                    prices are per person. The page aims to inform renters and bring awareness to renting affordability. 
+                </p>
+
+                <p className="source">
+                    <b>Data Source:</b>
+                    <a
+                    href="https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1110019001"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Statistics Canada (StatsCan)
+                    </a>
+                    .
+                </p>
+
+                <h3>What You'll Find on This Page</h3>
+                <ul>
+                    <li>
+                        <b>Income and Rent Growth Comparison: </b>
+                        Compares income and rent growth by using a calculated index. 
+                        The index was calculated by using 2019 values as baseline. </li>
+                    <li>
+                        <b>Average Percentage of Income Spent on Yearly Rent: </b>
+                        View how percentage of income spent on rent changes over years 
+                        and compares to the affordability threshold. 
+                        Yearly rent was calculated to compare to yearly income.</li>
+                    <li>Key insights into affordability for rental housing.</li>
+                </ul>
+            </div>
+
+            <div className="panel panel-content">
+                <tableau-viz id="tableauViz"
+                    style={{ width: "100%", height: "100vh" }}
+                    device="desktop"       
+                    src='https://public.tableau.com/views/IncomeAnalysis_17557250314520/Dashboard'
+                    toolbar="bottom"
+                    hide-tabs>
+                    </tableau-viz>
+            </div>
+            <div className="panel panel-content">
+                <h3>Key Insights:</h3>
+                <ul>
+                    <li>key insight 1</li>
+                    <li>key insight 2</li>
+                    <li>key insight 3</li>
+                </ul>
+            </div>
+        </section>
+    );
 }
